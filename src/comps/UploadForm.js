28c4f6cdc3593b852bc61a38c45e9ceb050f47a2
Laura THAY <laura.thay@ipag.fr>
2022-06-06 22:43:29 +0200

    error init

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -4,24 +4,30 @@ const UploadForm = () => {
 
     //download the file, null cause need to click
     const [file, setFile] = useState(null);
+    //for errors
+    const [error, setError] = useState(null);
 
     //array of allowed type
-    const allowedType = [
-        
-    ]
+    const types = [ 'image/png' , 'image/jpeg' ];
 
     //we get the file
     const changeHandler = (e) => {
         let selected = e.target.files[0];
 
-        if (selected){
-            setFile(selected)
+        if (selected && types.includes(selected.type)){
+            setFile(selected);
+        } else {
+            setFile(null);
+            setError('set the right type of file');
         }
     }
 
   return (
       <form action="">
           <input type="file" onClick={changeHandler}/>
+          <div className="output">
+              { error && <div className='error'> {error} </div>}
+          </div>
       </form>
   )
 }
